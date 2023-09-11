ada183acc044b302f9cb272dfc6a2eb74feeb5f3
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2023-09-11 04:36:27 +0000

    setup

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,5 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+    return <div className="bg-red-500">test</div>;
+}
---
+++ components/navBar.tsx
@@ -0,0 +1,8 @@
+import { ModeToggle, ButtonToggle } from "@/components/toggle-theme";
+export function NavBar() {
+    return (
+        <div className="fixed top-0 w-full bg-indigo-500 p-4">
+            <ButtonToggle />
+        </div>
+    );
+}
